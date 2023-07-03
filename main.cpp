2fc5e18332e5ab9320128a33aca559bc623fc30a
symcreg <symcreg@outlook.com>
2023-07-03 12:50:40 +0800

    压缩图片

--- main.cpp
+++ main.cpp
@@ -4,15 +4,24 @@ using namespace cv;
 using namespace std;
 
 //读入图片并转换为灰度图
-Mat baseImgGrey(){
-	Mat img,imgGray;
-	img=imread("0.jpg");
+Mat baseImgGrey(Mat &img){
+	Mat imgGray=Mat::zeros(img.size(),img.type());
 	if(img.empty()){
 		cout<<"Error: Image cannot be loaded"<<endl;
 		return Mat();
 	}
 	cvtColor(img,imgGray,COLOR_BGR2GRAY);
 	return imgGray;
+}
+//压缩图片
+Mat Compress(Mat &imgGray){
+	Mat imgCompress=Mat::zeros(imgGray.rows/2,imgGray.cols/2,imgGray.type());
+	for(int i=0;i<imgGray.rows;i+=2){
+		for(int j=0;j<imgGray.cols;j+=2){
+			imgCompress.at<uchar>(i/2,j/2)=(imgGray.at<uchar>(i,j)+imgGray.at<uchar>(i+1,j)+imgGray.at<uchar>(i,j+1)+imgGray.at<uchar>(i+1,j+1))/4;//取2x2像素块四个点的平均值
+		}
+	}
+	return imgCompress;
 }
 //固定阈值分割
 Mat threshold(Mat &imgGray,int thresh){
@@ -53,7 +62,7 @@ Mat adaptiveThreshold(Mat &imgGray){
 	return imgThreshold;
 }
 //腐蚀
-Mat erode(Mat imgThreshold){
+Mat erode(Mat &imgThreshold){
 	Mat imgErode=Mat::zeros(imgThreshold.size(),imgThreshold.type());
 	for(int i=0;i<imgThreshold.rows;i++){
 		for(int j=0;j<imgThreshold.cols;j++){
@@ -66,17 +75,55 @@ Mat erode(Mat imgThreshold){
 			}else{
 				imgErode.at<uchar>(i,j)=0;
 			}
+			// if(imgThreshold.at<uchar>(i,j)==0){
+			// 	if(imgThreshold.at<uchar>(i-1,j)==255||imgThreshold.at<uchar>(i+1,j)==255||imgThreshold.at<uchar>(i,j-1)==255||imgThreshold.at<uchar>(i,j+1)==255){
+			// 		imgErode.at<uchar>(i,j)=255;
+			// 	}else{
+			// 		imgErode.at<uchar>(i,j)=0;
+			// 	}
+			// }else{
+			// 	imgErode.at<uchar>(i,j)=0;
+			// }
 		}
 	}
 	return imgErode;
 }
 int main(){
 
-	Mat imgGray=baseImgGrey();
-	Mat imgThreshold=adaptiveThreshold(imgGray);
-	// Mat imgErode=erode(imgThreshold);
-	imshow("Image",imgThreshold);
+	Mat img=imread("0.jpg");
+	Mat imgGray=baseImgGrey(img);
+	Mat imgThreshold=threshold(imgGray,130);
+	Mat imgErode=erode(imgThreshold);
+	imshow("Image1",imgErode);
 	waitKey(0);
+
+	Mat imgCompress=Compress(imgGray);
+	Mat imgThreshold2=threshold(imgCompress,130);
+	Mat imgErode2=erode(imgThreshold2);
+
+	imshow("Image2",imgThreshold2);
+	waitKey(0);
+
+	// VideoCapture cap;
+	// cap.open(0);
+	// Mat frame;
+	// Mat frameCompress;
+	// Mat frameGray;
+	// Mat frameErode;
+	// Mat frameThreshold;
+	// while (1){
+	// 	cap>>frame;
+	// 	frameCompress=Compress(frame);
+	// 	frameGray=baseImgGrey(frameCompress);
+	// 	frameErode=erode(frameGray);
+	// 	frameThreshold=threshold(frameErode,10);
+	// 	imshow("frame",frameThreshold);
+	// 	if(waitKey(30)>=0) {
+	// 		break;
+	// 	}
+	// }
+	// cap.release();
+	
 
 
 
